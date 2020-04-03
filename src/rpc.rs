romanz/electrs

Don't warn when PeriodicUpdate cannot be sent

src/rpc.rs
@@ -495,7 +495,7 @@ impl RPC {
                     Notification::Periodic => {
                         for (i, sender) in senders.iter() {
                             if let Err(e) = sender.try_send(Message::PeriodicUpdate) {
-                                warn!("failed to send PeriodicUpdate to peer {}: {}", i, e);
+                                debug!("failed to send PeriodicUpdate to peer {}: {}", i, e);
                             }
                         }
                     }

BitcoinUnlimited/ElectrsCash

src/rpc.rs
@@ -679,14 +679,14 @@ impl RPC {
                     Notification::ScriptHashChange(hash) => {
                         for (i, sender) in senders.iter() {
                             if let Err(e) = sender.try_send(Message::ScriptHashChange(hash)) {
-                                warn!("failed to send ScriptHashChange to peer {}: {}", i, e);
+                                debug!("failed to send ScriptHashChange to peer {}: {}", i, e);
                             }
                         }
                     }
                     Notification::ChainTipChange(hash) => {
                         for (i, sender) in senders.iter() {
                             if let Err(e) = sender.try_send(Message::ChainTipChange(hash.clone())) {
-                                warn!("failed to send ChainTipChange to peer {}: {}", i, e);
+                                debug!("failed to send ChainTipChange to peer {}: {}", i, e);
                             }
                         }
                     }